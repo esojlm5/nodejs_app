309ccd5f0c275ee67cd8e38b72ba1b4a3e570aa2
jose <jose@joses-MacBook-Pro.local>
2019-12-09 09:27:04 -0500

    update get products

--- models/product.js
+++ models/product.js
@@ -1,17 +1,31 @@
 const fs = require('fs');
 const path = require('path');
 
+const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  })
+};
+
 module.exports = class Product {
   constructor(t) {
     this.title = t;
   }
   save() {
-    const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
-    fs.readFile(p, (err, content) => {
-      console.log(content);
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      })
     });
   }
 
-  static fetchAll() {
-  }
-}
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  };
+};
